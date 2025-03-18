d66d920a65d6d7553de2504a1bf4d703103aeff9
aleksandr.blokhin <aleksandr.blokhin@epita.fr>
2025-03-18 21:22:00 +0100

    Final working version with middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+
+app.use((req, res, next) => {
+    console.log("Time:", Date.now()); 
+    next();
 });
+
+
+app.use((req, res, next) => {
+    const num1 = 4;
+    const num2 = 7;
+    req.calculatedValue = num1 * num2;
+    console.log(`Calculated Value: ${req.calculatedValue}`); 
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send(`Hello World! Calculated Value: ${req.calculatedValue}`);
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
