4f48efc385ed05e57ff090f5f05a143c770d6d67
aleksandr.blokhin <aleksandr.blokhin@epita.fr>
2025-03-21 21:58:14 +0100

    Finished modularity setup

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 const express = require("express");
+const test = require("./test"); // Импортируем test.js
+
 const app = express();
 const port = 3000;
 
-
-
+// Middleware с тестовой функцией
 app.use((req, res, next) => {
-    console.log("Time:", Date.now()); 
+    test(); // Вызываем test()
+    const now = Date.now();
+    req.requestTime = now;
+    console.log("Time:", now);
     next();
 });
 
-
+// Middleware с арифметикой
 app.use((req, res, next) => {
     const num1 = 4;
     const num2 = 7;
     req.calculatedValue = num1 * num2;
-    console.log(`Calculated Value: ${req.calculatedValue}`); 
+    console.log(`Calculated Value: ${req.calculatedValue}`);
     next();
 });
 
 app.get("/", (req, res) => {
-    res.send(`Hello World! Calculated Value: ${req.calculatedValue}`);
+    res.send(`Hello World! Calculated Value: ${req.calculatedValue}, Request Time: ${req.requestTime}`);
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
