d52c0f51dec98cb3a58feba5a0d1bfae964b6ba5
aleksandr.blokhin <aleksandr.blokhin@epita.fr>
2025-03-21 23:09:22 +0100

    Added routes, POST methods, and JSON parser

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-// Получить список пользователей
 router.get("/", (req, res) => {
     res.send("Users page");
 });
 
-// Добавить нового пользователя
 router.post("/", (req, res) => {
+    console.log(req.body); 
     res.send("User added");
 });
 
-// Обновить информацию пользователя
 router.put("/:id", (req, res) => {
     res.send(`User with ID ${req.params.id} updated`);
 });
 
-// Удалить пользователя
 router.delete("/:id", (req, res) => {
     res.send(`User with ID ${req.params.id} deleted`);
 });
