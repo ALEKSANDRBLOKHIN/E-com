bb26105848240ef5c77a5bf4c0a1e736e9f2ca82
aleksandr.blokhin <aleksandr.blokhin@epita.fr>
2025-03-22 02:09:16 +0100

    Refactor userSignUp to return limited response fields

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,15 +1,34 @@
+const User = require("../models/userModel");
+
 exports.userLogIn = (req, res) => {
-    res.send("User login");
+  res.send("User login");
 };
 
-exports.userSignUp = (req, res) => {
-    const { firstName, email } = req.body;
-    const hashedPassword = req.hashedPassword; 
+exports.userSignUp = async (req, res) => {
+  try {
+    const { firstName, email, lastName, imageUrl, role } = req.body;
+    const hashedPassword = req.hashedPassword;
 
-    res.json({
-        firstName,
-        email,
-        hashedPassword,
-        _id: "randomId4567"
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      imageUrl,
+      role,
+      inventory: [],
     });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json({
+        firstName: savedUser.firstName,
+        email: savedUser.email,
+        role: savedUser.role
+      });
+      
+  } catch (error) {
+    console.error("Ошибка при создании пользователя:", error);
+    res.status(500).json({ error: "Ошибка сервера при создании пользователя" });
+  }
 };
---
+++ src/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  inventory: {
+    type: Array,
+    required: false, 
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
