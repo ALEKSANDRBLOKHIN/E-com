5a17407dfe350586493b16be9c0313e238114ecf
aleksandr.blokhin <aleksandr.blokhin@epita.fr>
2025-03-22 01:16:55 +0100

    Bag fix

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config();  
+dotenv.config();
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("DB_CONNECTION:", process.env.DB_CONNECTION);
 
 const isProduction = process.env.NODE_ENV === "production";
-
-
 const mongoConnection = isProduction ? process.env.DB_CONNECTION : process.env.LOCAL_DB_CONNECTION;
+
+console.log(" Using database:", mongoConnection); // Проверка
 
 mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongoConnection);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(mongoConnection, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(" Connected to MongoDB");
     } catch (error) {
-        console.error("Connection failed!", error);
+        console.error(" Connection failed!", error);
         process.exit(1);
     }
 };
